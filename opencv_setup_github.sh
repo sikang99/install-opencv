68f9774cbbbb84bb9b11008a7303165194c80671
Stoney Kang <sikang99@gmail.com>
2018-08-05 21:55:02 +0900

    add shell scripts

---
+++ opencv_setup_github.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VERSION=github
+
+rm -rf opencv_build_$(VERSION)
+mkdir opencv_build_$(VERSION)
+cd opencv_build_$(VERSION) 
+
+git clone https://github.com/opencv/opencv.git
+git clone https://github.com/opencv/opencv_contrib.git
+
+mkdir build
+cd build
+
+cmake \
+    -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_C_EXAMPLES=ON \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
+    -D BUILD_EXAMPLES=ON ../opencv
+
+make -j4
+
